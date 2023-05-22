c3473c3906b416177057991e62df664c5012bc18
Ezedrex <ezzbenig24@gmail.com>
2023-05-22 17:07:08 +0800

    added categs expense and income

--- src/budgee/SetBudget.java
+++ src/budgee/SetBudget.java
@@ -42,7 +42,7 @@ public class SetBudget extends JFrame {
 	 * Create the frame.
 	 */
 	public SetBudget() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 444, 415);
 		SetBudgetPanel = new JPanel();
 		SetBudgetPanel.setBackground(new Color(66, 83, 109));
@@ -52,14 +52,15 @@ public class SetBudget extends JFrame {
 		SetBudgetPanel.setLayout(null);
 		
 		JLabel lblSetBudget = new JLabel("Set Budget");
+		lblSetBudget.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSetBudget.setForeground(new Color(252, 187, 129));
-		lblSetBudget.setFont(new Font("Tahoma", Font.BOLD, 30));
-		lblSetBudget.setBounds(124, 11, 173, 48);
+		lblSetBudget.setFont(new Font("Quicksand Light", Font.BOLD, 30));
+		lblSetBudget.setBounds(130, 11, 173, 48);
 		SetBudgetPanel.add(lblSetBudget);
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(66, 83, 109));
-		panel.setBounds(52, 70, 332, 101);
+		panel.setBounds(50, 70, 332, 101);
 		SetBudgetPanel.add(panel);
 		panel.setLayout(null);
 		
@@ -68,28 +69,30 @@ public class SetBudget extends JFrame {
         // Set the border for the panel
         panel.setBorder(border);
 		
-		JLabel SetCateg = new JLabel("Baby");
+		JLabel SetCateg = new JLabel("BILLS");
 		SetCateg.setForeground(new Color(252, 187, 129));
 		SetCateg.setHorizontalAlignment(SwingConstants.CENTER);
-		SetCateg.setFont(new Font("Tahoma", Font.BOLD, 35));
-		SetCateg.setBounds(10, 28, 312, 43);
+		SetCateg.setFont(new Font("Quicksand Light", Font.BOLD, 35));
+		SetCateg.setBounds(10, 35, 312, 43);
 		panel.add(SetCateg);
 		
 		JLabel lblCateg = new JLabel("Category");
+		lblCateg.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCateg.setForeground(new Color(85, 111, 146));
 		lblCateg.setBackground(new Color(85, 111, 146));
-		lblCateg.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblCateg.setBounds(125, 11, 69, 19);
+		lblCateg.setFont(new Font("Quicksand Light", Font.BOLD, 15));
+		lblCateg.setBounds(130, 11, 69, 19);
 		panel.add(lblCateg);
 		
 		JLabel lblLimit = new JLabel("Limit");
+		lblLimit.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLimit.setForeground(new Color(252, 187, 129));
-		lblLimit.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblLimit.setFont(new Font("Quicksand Light", Font.BOLD, 20));
 		lblLimit.setBounds(52, 204, 60, 31);
 		SetBudgetPanel.add(lblLimit);
 		
 		SetLimit = new JTextField();
-		SetLimit.setFont(new Font("Tahoma", Font.BOLD, 15));
+		SetLimit.setFont(new Font("Quicksand Light", Font.BOLD, 15));
 		SetLimit.setForeground(new Color(252, 187, 129));
 		SetLimit.setBackground(new Color(66, 83, 109));
 		SetLimit.setBounds(115, 198, 269, 48);
@@ -99,16 +102,18 @@ public class SetBudget extends JFrame {
 		SetLimit.setBorder(border);
 		
 		JLabel lblMonth = new JLabel("Month:");
+		lblMonth.setHorizontalAlignment(SwingConstants.CENTER);
 		lblMonth.setBackground(new Color(85, 111, 146));
 		lblMonth.setForeground(new Color(85, 111, 146));
-		lblMonth.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblMonth.setFont(new Font("Quicksand Light", Font.BOLD, 20));
 		lblMonth.setBounds(115, 268, 71, 31);
 		SetBudgetPanel.add(lblMonth);
 		
 		JLabel SetMonth = new JLabel("May, 2023");
+		SetMonth.setHorizontalAlignment(SwingConstants.CENTER);
 		SetMonth.setBackground(new Color(85, 111, 146));
 		SetMonth.setForeground(new Color(85, 111, 146));
-		SetMonth.setFont(new Font("Tahoma", Font.BOLD, 20));
+		SetMonth.setFont(new Font("Quicksand Light", Font.BOLD, 20));
 		SetMonth.setBounds(196, 268, 113, 31);
 		SetBudgetPanel.add(SetMonth);
 		
@@ -118,13 +123,14 @@ public class SetBudget extends JFrame {
 			}
 		});
 		btnCancel.setForeground(new Color(252, 187, 129));
-		btnCancel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnCancel.setFont(new Font("Quicksand Light", Font.BOLD, 15));
 		btnCancel.setBackground(new Color(66, 83, 109));
 		btnCancel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		btnCancel.setBounds(85, 310, 130, 40);
 		SetBudgetPanel.add(btnCancel);
 		
-		JButton btnCancel_1 = new JButton("New button");
+		JButton btnCancel_1 = new JButton("Set");
+		btnCancel_1.setFont(new Font("Quicksand Light", Font.BOLD, 15));
 		btnCancel_1.setForeground(new Color(252, 187, 129));
 		btnCancel_1.setBackground(new Color(66, 83, 109));
 		btnCancel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
